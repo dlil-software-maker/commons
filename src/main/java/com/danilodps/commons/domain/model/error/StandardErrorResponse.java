1179fc6b86615ef2711b52d3f16047d1ceed4ce7
Danilo-dps <danilodpsnilo@outlook.com>
2026-02-18 19:02:59 -0300

    feat: um padrão de exception

---
+++ src/main/java/com/danilodps/commons/domain/model/error/StandardErrorResponse.java
@@ -0,0 +1,19 @@
+package com.danilodps.commons.domain.model.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StandardErrorResponse {
+    private HttpStatus statusCode;
+    private String errorType;
+    private List<StandardErrorDetails> standardErrorDetailsList;
+}
