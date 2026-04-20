21a6e658fedb15b3fdcb7cdb1f460f93afa4129f
Danilo Pereira da Silva <dlil.testes.software@gmail.com>
2026-04-20 14:47:19 -0300

    fix: remoção da interface desnecessária

--- src/main/java/com/danilodps/commons/domain/model/response/ErrorResponse.java
+++ src/main/java/com/danilodps/commons/domain/model/response/ErrorResponse.java
@@ -5,5 +5,5 @@ import java.time.LocalDateTime;
 public record ErrorResponse(LocalDateTime timestamp,
                             String message,
                             String errorType,
-                            int statusCode) implements MessageKafka{
+                            int statusCode) {
 }
